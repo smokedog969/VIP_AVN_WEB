338bae9a83c36ea9923a1b5c130999c5b4a13ed1
smokedog969 <steffen.schafrik@googlemail.com>
2021-03-27 11:01:07 +0100

    handling text changed

--- inc/service.php
+++ inc/service.php
@@ -1,3 +1,4 @@
+<!--
 <div class="service" id="service">
     <div class="container">
         <div class="row">
@@ -20,7 +21,66 @@
             </div>
             <div class="four columns">
                 <div class="service-icon">
-                    <!-- <i class="fa fa-spinner" aria-hidden="true"></i> -->
+                    
+                    <i class="fa fa-plane" aria-hidden="true"></i>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+-->
+
+<!--
+<div class="service service-grey">
+    <div class="container">
+        <div class="row">
+            <div class="four columns">
+                <div class="service-icon">
+                    <i class="fa fa-user-tie"></i>
+                </div>
+            </div>
+            <div class="eight columns">
+                <h1 class="brand-title">Passenger <strong>Handling</strong></h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+                    labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+                    et ea rebum.
+                </p>
+
+
+            </div>
+        </div>
+    </div>
+</div>
+-->
+<!-- NEU -->
+
+<div class="service" id="service">
+    <div class="container">
+        <div class="row">
+            <div class="eight columns">
+                <h1 class="service-title">PILOTS & CREW <strong>Service</strong></h1>
+                <p>
+                    We manage every aspect of your departure and arrival, together with your crew's total comfort and
+                    convenience.
+                    You and the crew will enjoy superior facilities in our lounge, which depending on the location,
+                    complimentary Wi-Fi together with refreshments.
+
+                    The VIP Aviation Handling team looks forward to welcoming you to the General Aviation Terminal.
+                </p>
+                <p>
+                    Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor
+                    sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+                    magna aliquyam erat, sed diam voluptua.
+                </p>
+                <p>
+                    At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+                    sanctus est Lorem ipsum dolor sit amet.
+                </p>
+            </div>
+            <div class="four columns">
+                <div class="service-icon">
+
                     <i class="fa fa-plane" aria-hidden="true"></i>
                 </div>
             </div>
@@ -40,11 +100,22 @@
                 </div>
             </div>
             <div class="eight columns">
-                <h1 class="brand-title">Passenger <strong>Handling</strong></h1>
+                <h1 class="brand-title">Executive Aircraft <strong>Handling</strong></h1>
                 <p>
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
-                    labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
-                    et ea rebum.
+                    VIP Aviation Handling stands for quality, comfort and first-class service.
+
+                    VIP AVIATION Handling is more that 30 years of experience at the Berlin Airports.
+
+                    In recent years we have provided professional, competitively priced ground handling services at the
+                    Berlin Airports to a wide variety of visitors from general aviation to VIP and corporate flights
+                    including politicians and celebrities.
+
+                    We have the facilities at Berlin Brandenburg International Airport and expertise to handle executive
+                    aircraft, and helicopters of every shape and size.
+
+                    Established in 1991, VIP Aviation has more than 30 years of experience in the aviation industry.
+                    Our goal is to provide you with the high quality services that we ourselves require from an Handling
+                    Agent. We look forward to welcoming you.
                 </p>
 
 
